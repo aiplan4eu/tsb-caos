ec2c770b1202935ed7e5c713f57ff7784e28c3e9
Greg <greg.kasapidis@gmail.com>
2022-12-13 15:46:21 +0200

    Improved planning model. Abstracted Planner to a separate class

--- main.py
+++ main.py
@@ -1,153 +1,35 @@
-import unified_planning
-from  unified_planning.shortcuts import *
+from planner import CAOSProblem
 
 #Data
-NumberOfClients = 3 #0,1 customers, #2 supplier
+NumberOfClients = 3 #0, 1 customers, #2 supplier
 NumberOfPeriods = 3
 StartBalance = 0
-
 
 #Transaction Data
 InboundContracts = [
     # Take 1000 from customer 0 at period 0 or 1500 on period 1
     {'id':0, 'client_id': 0, 'period': 0, 'amount': 1000, 'rate'  : 1.5  },
-    {'id':1, 'client_id': 1, 'period': 1, 'amount': 500,  'rate'  : 2.0  },
-    {'id':2, 'client_id': 0, 'period': 2, 'amount': 500,  'rate'  : 1.2  },
-    {'id':3, 'client_id': 1, 'period': 2, 'amount': 500,  'rate'  : 1.0  }
+    {'id':1, 'client_id': 1, 'period': 1, 'amount': 500,  'rate'  : 2.0  }
+    #{'id':2, 'client_id': 0, 'period': 2, 'amount': 500,  'rate'  : 1.2  },
+    #{'id':3, 'client_id': 1, 'period': 2, 'amount': 500,  'rate'  : 1.0  }
 ]
 
 
 OutboundContracts = [
-    {'id':4, 'client_id': 2, 'period': 2, 'amount': 2000, 'rate' : 1.0},
-    {'id':5, 'client_id': 2, 'period': 2, 'amount': 500,  'rate' : 1.2}
+    {'id':4, 'client_id': 2, 'period': 0, 'amount': 1000, 'rate' : 1.0 },
+    {'id':5, 'client_id': 2, 'period': 0, 'amount': 1500,  'rate' : 1.2 }
 ]
 
 
-#Model
-Period = UserType('Period')
-Client = UserType('Client')
-InContract = UserType('InContract')
-OutContract = UserType('OutContract')
-
-current_balance = unified_planning.model.Fluent('balance_current', IntType())
-balance_at = unified_planning.model.Fluent('balance', IntType(), p=Period)
-credit_line_charge = unified_planning.model.Fluent('credit_line_charge', IntType(), p=Period)
-direct_inbound_data = unified_planning.model.Fluent('direct_inbound_data', IntType(), p=Period, c=InContract)
-
-direct_outbound_data = unified_planning.model.Fluent('direct_outbound_data', IntType(), p=Period, c=OutContract)
-in_contract_status = unified_planning.model.Fluent('in_contract_status', BoolType(), p=InContract)
-out_contract_status = unified_planning.model.Fluent('out_contract_status', BoolType(), p=OutContract)
-
-
-#Problem
-problem = unified_planning.model.Problem('caos')
-problem.add_fluent(balance_at, default_initial_value = 0)
-problem.add_fluent(credit_line_charge, default_initial_value = 0)
-problem.add_fluent(direct_inbound_data, default_initial_value = -1)
-problem.add_fluent(direct_outbound_data, default_initial_value = -1)
-problem.add_fluent(in_contract_status, default_initial_value = False)
-problem.add_fluent(out_contract_status, default_initial_value = False)
-
-
-#Initialize
-clients = []
-periods = []
-in_contracts = []
-out_contracts = []
-for i in range(NumberOfPeriods):
-    periods.append(unified_planning.model.Object('period_%s' % i, Period))
-
-for i in range(NumberOfClients):
-    clients.append(unified_planning.model.Object('client_%s' % i, Client))
-
-problem.add_objects(periods)
-problem.add_objects(clients)
+#Generate Problem
+p = CAOSProblem(NumberOfClients, NumberOfPeriods, StartBalance)
 
 for c in InboundContracts:
-    start_p = c['period']
-    amount = c['amount']
-    rate = c['rate']
-    contract = unified_planning.model.Object('contract_%s' % c['id'], InContract)
-    in_contracts.append(contract)
-    problem.add_object(contract)
-    
-    for p in range(NumberOfPeriods):
-        if (p >= start_p):
-            problem.set_initial_value(direct_inbound_data(periods[p], contract), (1 + (p - start_p)) *amount)
-
+    p.AddInboundContract(c)
 
 for c in OutboundContracts:
-    start_p = c['period']
-    amount = c['amount']
-    rate = c['rate']
-    contract = unified_planning.model.Object('contract_%s' % c['id'], OutContract)
-    out_contracts.append(contract)
-    problem.add_object(contract)
-    
-    for p in range(NumberOfPeriods):
-        if (p >= start_p):
-            problem.set_initial_value(direct_outbound_data(periods[p], contract), (1 + (p - start_p)) * amount)
-    
+    p.AddOutboundContract(c)
 
-#Actions
-
-#Action A: Receive On Time
-direct_recv_action = unified_planning.model.InstantaneousAction('direct_recv_action', at_period=Period, base_contract=InContract)
-at_period = direct_recv_action.parameter('at_period')
-rel_contract = direct_recv_action.parameter('base_contract')
-direct_recv_action.add_precondition(Not(in_contract_status(rel_contract)))
-direct_recv_action.add_precondition(GT(direct_inbound_data(at_period, rel_contract), 0))
-direct_recv_action.add_effect(balance_at(at_period), Plus(balance_at(at_period), direct_inbound_data(at_period, rel_contract)))
-direct_recv_action.add_effect(in_contract_status(rel_contract), True)
-problem.add_action(direct_recv_action)
-
-#Action C: Pay On Time
-direct_pay_action = unified_planning.model.InstantaneousAction('direct_pay_action', at_period=Period, base_contract=OutContract)
-at_period = direct_pay_action.parameter('at_period')
-rel_contract = direct_pay_action.parameter('base_contract')
-direct_pay_action.add_precondition(Not(out_contract_status(rel_contract)))
-direct_pay_action.add_precondition(GT(direct_outbound_data(at_period, rel_contract), 0))
-direct_pay_action.add_effect(balance_at(at_period), Minus(balance_at(at_period), direct_outbound_data(at_period, rel_contract)))
-direct_pay_action.add_effect(out_contract_status(rel_contract), True)
-problem.add_action(direct_pay_action)
-
-#Action E: Credit Line Charge
-credit_line_charge_action = unified_planning.model.InstantaneousAction('credit_line_charge_action', at_period=Period)
-at_period = credit_line_charge_action.parameter('at_period')
-credit_line_charge_action.add_precondition(LT(balance_at(at_period), 0))
-credit_line_charge_action.add_effect(credit_line_charge(at_period), balance_at(at_period)) #first add to charge line
-credit_line_charge_action.add_effect(balance_at(at_period), 0) #then reset the balance
-problem.add_action(credit_line_charge_action)
-
-#Initial Balance
-problem.set_initial_value(balance_at(periods[0]), StartBalance)
-
-#Objective
-
-#Make sure that there is a non-negative balance every period
-for p in periods:
-    problem.add_goal(GE(balance_at(p), 0))
-
-#Make sure that all contracts have been addressed
-for c in in_contracts:
-    problem.add_goal(in_contract_status(c))
-
-for c in out_contracts:
-    problem.add_goal(out_contract_status(c))
-
-print(problem)
-
-#Solve
-with OneshotPlanner(problem_kind=problem.kind) as planner:
-    result = planner.solve(problem)
-    if result.status == up.engines.PlanGenerationResultStatus.SOLVED_SATISFICING:
-        print("Output Schedule using", planner.name)
-        for a in result.plan.actions:
-            print(a)
-    else:
-        print("No plan found.")
-
-
-
-
+#Solve the Problem
+p.Solve()
 
---
+++ planner.py
@@ -0,0 +1,225 @@
+import unified_planning
+from unified_planning.shortcuts import *
+from unified_planning.engines import SequentialSimulator
+from unified_planning.model import UPCOWState, Fluent
+from typing import cast
+
+class CAOSProblem:
+    def __init__(self, numberOfClients, numberOfPeriods, startBalance):
+        self.NumberOfClients = numberOfClients
+        self.NumberOfPeriods = numberOfPeriods
+        self.StartBalance = startBalance
+        self.InboundContracts = []
+        self.OutboundContracts = []
+        
+
+    def AddInboundContract(self, c):
+        if not c in self.InboundContracts:
+            self.InboundContracts.append(c)
+
+    def AddOutboundContract(self, c):
+        if not c in self.OutboundContracts:
+            self.OutboundContracts.append(c)
+    
+    def Report(self):
+        print("NOT IMPLEMENTED")
+        pass
+    
+    def Solve(self):
+
+        #Model
+        Period = UserType('Period')
+        Client = UserType('Client')
+        InContract = UserType('InContract')
+        OutContract = UserType('OutContract')
+
+        current_period = Fluent('current_period', BoolType(), p=Period)
+        connected_periods = Fluent('connected_periods', BoolType(), from_period=Period, to_period=Period)
+        start_balance_at = Fluent('start_balance_at', RealType(), p=Period)
+        final_balance_at = Fluent('final_balance_at', RealType(), p=Period)
+        #credit_line_charge = Fluent('credit_line_charge', IntType(), p=Period)
+        direct_inbound_data = Fluent('direct_inbound_data', RealType(), p=Period, c=InContract)
+
+        direct_outbound_data = Fluent('direct_outbound_data', RealType(), p=Period, c=OutContract)
+        in_contract_status = Fluent('in_contract_status', BoolType(), p=InContract)
+        out_contract_status = Fluent('out_contract_status', BoolType(), p=OutContract)
+
+
+        #Problem
+        problem = unified_planning.model.Problem('caos')
+        problem.add_fluent(current_period, default_initial_value = False)
+        problem.add_fluent(connected_periods, default_initial_value = False)
+        problem.add_fluent(start_balance_at, default_initial_value = 0)
+        problem.add_fluent(final_balance_at, default_initial_value = 0)
+        #problem.add_fluent(credit_line_charge, default_initial_value = 0)
+        problem.add_fluent(direct_inbound_data, default_initial_value = -1)
+        problem.add_fluent(direct_outbound_data, default_initial_value = -1)
+        problem.add_fluent(in_contract_status, default_initial_value = False)
+        problem.add_fluent(out_contract_status, default_initial_value = False)
+
+        #Initialize
+        clients = []
+        periods = []
+        in_contracts = []
+        out_contracts = []
+        for i in range(self.NumberOfPeriods + 1):
+            periods.append(unified_planning.model.Object('period_%s' % i, Period))
+
+        for i in range(self.NumberOfClients):
+            clients.append(unified_planning.model.Object('client_%s' % i, Client))
+
+        problem.add_objects(periods)
+        problem.add_objects(clients)
+
+
+        #Init Period Connections
+        problem.set_initial_value(current_period(periods[0]), True)
+        for i in range(self.NumberOfPeriods):
+            problem.set_initial_value(connected_periods(periods[i], periods[i + 1]), True)
+
+        for c in self.InboundContracts:
+            start_p = c['period']
+            amount = c['amount']
+            rate = c['rate']
+            contract = unified_planning.model.Object('incontract_%s' % c['id'], InContract)
+            in_contracts.append(contract)
+            problem.add_object(contract)
+            
+            for p in range(self.NumberOfPeriods):
+                if (p >= start_p):
+                    problem.set_initial_value(direct_inbound_data(periods[p], contract), (1.0 + rate * (p - start_p)) * amount)
+
+
+        for c in self.OutboundContracts:
+            start_p = c['period']
+            amount = c['amount']
+            rate = c['rate']
+            contract = unified_planning.model.Object('outcontract_%s' % c['id'], OutContract)
+            out_contracts.append(contract)
+            problem.add_object(contract)
+            
+            for p in range(self.NumberOfPeriods):
+                if (p >= start_p):
+                    problem.set_initial_value(direct_outbound_data(periods[p], contract), (1.0 + rate * (p - start_p)) * amount)
+            
+        #Initial Balance
+        problem.set_initial_value(start_balance_at(periods[0]), self.StartBalance)
+        problem.set_initial_value(final_balance_at(periods[0]), self.StartBalance)
+
+        #Actions
+
+
+        #Action A: Execute Receiving Contract
+        direct_recv_action = unified_planning.model.InstantaneousAction('direct_recv_action', at_period=Period, base_contract=InContract)
+        at_period = direct_recv_action.parameter('at_period')
+        rel_contract = direct_recv_action.parameter('base_contract')
+        direct_recv_action.add_precondition(Not(in_contract_status(rel_contract))) #Ensure that the contract has not been executed already
+        direct_recv_action.add_precondition(current_period(at_period)) #Make sure the contract is executed in the current period
+        direct_recv_action.add_precondition(GT(direct_inbound_data(at_period, rel_contract), 0))
+        direct_recv_action.add_increase_effect(final_balance_at(at_period), direct_inbound_data(at_period, rel_contract))
+        direct_recv_action.add_effect(in_contract_status(rel_contract), True)
+        problem.add_action(direct_recv_action)
+
+
+        #Action B: Execute Payment Contract
+        direct_pay_action = unified_planning.model.InstantaneousAction('direct_pay_action', at_period=Period, base_contract=OutContract)
+        at_period = direct_pay_action.parameter('at_period')
+        rel_contract = direct_pay_action.parameter('base_contract')
+        direct_pay_action.add_precondition(Not(out_contract_status(rel_contract))) #Ensure that the contract has not been executed already
+        direct_pay_action.add_precondition(current_period(at_period)) #Make sure the contract is executed in the current period
+        direct_pay_action.add_precondition(GT(direct_outbound_data(at_period, rel_contract), 0))
+        direct_pay_action.add_decrease_effect(final_balance_at(at_period), direct_outbound_data(at_period, rel_contract))
+        direct_pay_action.add_effect(out_contract_status(rel_contract), True)
+        problem.add_action(direct_pay_action)
+
+
+        #Action D: Advance Period
+        period_advance_action = unified_planning.model.InstantaneousAction('advance_period', from_period=Period, to_period=Period)
+        from_period = period_advance_action.parameter('from_period')
+        to_period = period_advance_action.parameter('to_period')
+        period_advance_action.add_precondition(connected_periods(from_period, to_period))
+        period_advance_action.add_precondition(current_period(from_period))
+        period_advance_action.add_effect(start_balance_at(to_period), Times(1.02, final_balance_at(from_period)), LT(final_balance_at(from_period), 0))
+        period_advance_action.add_effect(final_balance_at(to_period), Times(1.02, final_balance_at(from_period)), LT(final_balance_at(from_period), 0))
+        period_advance_action.add_effect(start_balance_at(to_period), final_balance_at(from_period), GT(final_balance_at(from_period), 0))
+        period_advance_action.add_effect(final_balance_at(to_period), final_balance_at(from_period), GT(final_balance_at(from_period), 0))
+
+        period_advance_action.add_effect(current_period(from_period), False)
+        period_advance_action.add_effect(current_period(to_period), True)
+        problem.add_action(period_advance_action)
+
+
+        #Constraints
+
+        #Make sure that there is a non-negative balance every period
+        #for p in periods:
+        #    problem.add_goal(GE(final_balance_at(p), 0))
+
+        #Make sure to advance to the final period
+        problem.add_goal(current_period(periods[-1]))
+
+        #Make sure that all contracts have been addressed
+        for c in in_contracts:
+            problem.add_goal(in_contract_status(c))
+
+        for c in out_contracts:
+            problem.add_goal(out_contract_status(c))
+
+        #Cost Minimization (NO SOLVER - RIP)
+
+        # problem.add_quality_metric(
+        #     unified_planning.model.metrics.MinimizeExpressionOnFinalState(credit_line_charge(periods[-1]))
+        # )
+
+        print(problem)
+
+        #Solve
+        # OLD
+        # with OneshotPlanner(
+        #     optimality_guarantee= unified_planning.engines.PlanGenerationResultStatus.SOLVED_OPTIMALLY) as planner:
+        #     result = planner.solve(problem)
+        #     if result.status == up.engines.PlanGenerationResultStatus.SOLVED_SATISFICING:
+        #         print("Output Schedule using", planner.name)
+        #         for a in result.plan.actions:
+        #             print(a)
+        #     else:
+        #         print("No plan found.")
+
+        #Solve
+        with OneshotPlanner(name = "enhsp-opt") as planner:
+            planner.skip_checks = False
+            result = planner.solve(problem)
+            print(result.status)
+            if result.status == unified_planning.engines.PlanGenerationResultStatus.SOLVED_OPTIMALLY or result.status == unified_planning.engines.PlanGenerationResultStatus.SOLVED_SATISFICING:
+                print("Output Schedule using", planner.name)
+                for a in result.plan.actions:
+                    print(a)
+                
+                print("Simulate the plan")
+                simulator = SequentialSimulator(problem)
+                state = UPCOWState(problem.initial_values)
+                for a in result.plan.actions:
+                    action_events = simulator.get_events(a.action, a.actual_parameters)
+                    for e in action_events:
+                        state = cast(UPCOWState, simulator.apply(e, state))
+                    
+                    #REPORT ACTION RESULTS
+                    for p in periods:
+                        if (state.get_value(current_period(p)) == problem.env.expression_manager.TRUE()):
+                            print('CURRENT PERIOD', p)
+                            print('Action', a)
+                            print('Start Balance', state.get_value(start_balance_at(p)))
+                            print('Final Balance', state.get_value(final_balance_at(p)))
+                            break
+                
+                if not (simulator.is_goal(state)):
+                    print("ERROR: NO GOAL STATE")
+            
+            else:
+                print("No plan found.")
+
+
+    
+
+
+        
